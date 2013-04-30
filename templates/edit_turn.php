f9652c023c9bdce0a9355c84040e83728eb7c436
Rasmus Fuhse <Krassmus@gmail.com>
2013-04-30 22:15:48 +0200

    add template

---
+++ templates/edit_turn.php
@@ -0,0 +1,15 @@
+<?
+$label = $turn->isNew() ? _("Name der neuen Spielrunde") : _("Name der Spielrunde");
+?>
+<form action="<?= PluginEngine::getLink($plugin, array(), 'edit_turn/'.$turn->getId()) ?>" method="post">
+    <input type="text" 
+           name="name" 
+           value="<?= htmlReady($turn['name']) ?>" 
+           style="width: 100%; font-size: 1.8em; font-weight: bold;"
+           placeholder="<?= $label ?>"
+           aria-label="<?= $label ?>">
+    <br>
+    <textarea name="description" style="width: 100%; height: 150px;"><?= htmlReady($turn['description']) ?></textarea>
+    <br>
+    <?= \Studip\Button::createAccept(_("Speichern"))?>
+</form>
